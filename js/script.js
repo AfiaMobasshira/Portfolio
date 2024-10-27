c6f67607c0a5ca32c6556127d60241d657dd2421
AfiaMobasshira <arthyafiya93@gmail.com>
2024-10-28 00:06:22 +0600

    add about section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+// dropdown menu //
 const toggleButton = document.querySelector("#toggleBtn");
 const dropdown = document.querySelector("#dropdown");
 
@@ -7,5 +7,15 @@ console.log(dropdown);
 toggleButton.addEventListener('click', ()=>{
     dropdown.classList.toggle('hidden');
 });
+// dropdown menu //
+
+//auto type //
+var typed = new Typed(".auto-input",{
+    strings: ["Afia Mobasshira", "Frontend Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+})
+//auto type //
 
   
